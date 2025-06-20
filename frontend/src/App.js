bb738dbd0e8766a28916100b6375112ded17b72e
JYH <jh794@duke.edu>
2025-06-20 13:09:28 +0800

    Add alert to confirm new code execution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import {
   PieChartOutlined,
 } from '@ant-design/icons';
 
+alert('This is the latest code! Build time: ' + new Date().toISOString());
+
 const { Header, Sider, Content } = Layout;
 
 // Build timestamp to force cache invalidation
